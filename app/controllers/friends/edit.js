819ff5699cd823aecc2ab5cfcc1820313326ec46
rabin <rabindranath.prithvi@gmail.com>
2015-11-25 19:58:58 +0530

    Edit friend done

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,6 +3,7 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel() {
+      this.get('model').rollback();
       this.transitionToRoute('friends.show', this.get('model'));
       return true;
     }
