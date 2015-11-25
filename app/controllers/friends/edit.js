ccda4fcde9829ab554823b6bff65f83f07a9c197
rabin <rabindranath.prithvi@gmail.com>
2015-11-25 18:57:10 +0530

    Edit complete

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,8 +3,8 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel() {
-      this.transitionToRoute('friends.index')
-      return false;
+      this.transitionToRoute('friends.show', this.get('model'));
+      return true;
     }
   }
 });
--- app/router.js
+++ app/router.js
@@ -8,9 +8,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('friends', function() {
     this.route('new');
-    this.route('show', {
-      path: ':friend_id'
-    });
+    this.route('show', { path: ':friend_id' });
+    this.route('edit', { path: ':friend_id/edit' });
   });
 });
 
