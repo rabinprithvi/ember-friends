3b783eab21d696be92ab835efb0ec860bb9c431f
rabin <rabindranath.prithvi@gmail.com>
2015-11-26 15:02:43 +0530

    added willtransition to revert the model

--- app/controllers/friends/edit.js
+++ app/controllers/friends/edit.js
@@ -3,7 +3,6 @@ import FriendsBaseController from './base';
 export default FriendsBaseController.extend({
   actions: {
     cancel() {
-      this.get('model').rollback();
       this.transitionToRoute('friends.show', this.get('model'));
       return true;
     }
--- app/routes/friends/edit.js
+++ app/routes/friends/edit.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    willTransition(){
+      let friend = this.controller.get('model');
+      friend.rollback();
+    }
+  }
+
 });
