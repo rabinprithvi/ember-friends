ffb498dc1aac0d8d520a5eafe9dc99c983de05c2
rabin <rabindranath.prithvi@gmail.com>
2015-11-27 14:25:17 +0530

    For production adapter

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,5 +1,7 @@
 import ActiveModeAdapter from 'active-model-adapter';
+import config from '../config/environment';
 
 export default ActiveModeAdapter.extend({
+	host: config.host,
   namespace: 'api'
 });
