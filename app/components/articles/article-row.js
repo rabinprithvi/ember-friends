e6132cb1bd72dba56956e8ba2f3277658164e1ec
Rabin <rabindranath.ideas2it@gmail.com>
2015-12-30 18:57:56 +0530

    Component to show articles added

---
+++ app/components/articles/article-row.js
@@ -0,0 +1,15 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'tr',
+  article: null, //passed-in
+  articleStatus: null, //passed-in
+  actions: {
+    saveArticle() {
+      let article = this.get('article');
+      if (article.get('hasDirtyAttributes')) {
+        this.sendAction('save', article);
+      }
+    }
+  }
+});
